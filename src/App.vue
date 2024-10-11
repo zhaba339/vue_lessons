a6898c4fa10a15635d72c054099200d70eab0d13
AleksandrGalinurov <alexgalinurov@gmail.com>
2024-10-11 16:41:41 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="app">
+    <post-form @create="createPost" />
+    <post-list :posts="posts" />
+  </div>
+</template>
+
+<script>
+import PostList from "@/components/PostList.vue";
+import PostForm from "@/components/PostForm.vue";
+
+export default {
+  components: {
+    PostList,
+    PostForm,
+  },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "JavaScript 1",
+          body: "Описание поста 1",
+        },
+        {
+          id: 2,
+          title: "JavaScript 2",
+          body: "Описание поста 2",
+        },
+        {
+          id: 3,
+          title: "JavaScript 3",
+          body: "Описание поста 3",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    createPost(post, second, third) {
+      console.log(post);
+      console.log(second);
+      console.log(third);
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <form onsubmit="return false">
+    <h4>Создание поста</h4>
+    <input
+      v-model="post.title"
+      class="input"
+      placeholder="Название"
+      type="text"
+      @input="post.title = $event.target.value"
+    />
+    <input
+      v-model="post.body"
+      class="input"
+      placeholder="Описание"
+      type="text"
+      @input="post.body = $event.target.value"
+    />
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post, "fdsf", "fdsfsdfdsf");
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
